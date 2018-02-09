360c3fed9c48619573b5373da10e5cc083a8a5d5
Dmitrii <dimana1507@gmail.com>
2018-02-09 14:42:27 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ window.onscroll = function () {
     var scrolled = window.pageYOffset || document.documentElement.scrollTop;
     if (scrolled > '99') {
         document.getElementById('header').classList.add('sticky__header');
+
     }
     else {
         document.getElementById('header').classList.remove('sticky__header');
